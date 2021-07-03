9dbd9d2e0bf540fb37581c58b13bae454af56fd8
Ilya Zlobintsev <ilya.zl@protonmail.com>
2021-07-03 09:57:28 +0300

    Add clarification

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rocket::fs::{FileServer, relative};
 
 #[rocket::main]
 async fn main() {
-
+    // I haven't figured out how to start this from `build.rs` without Cargo hanging, so it's here.
     #[cfg(debug_assertions)]
     Command::new("npm")
         .current_dir(env!("CARGO_MANIFEST_DIR"))
